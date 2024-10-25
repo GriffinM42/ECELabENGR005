da0429a4d513ee172639269388963b047f6ae5c5
GriffinM42 <grm228@lehigh.edu>
2024-10-25 13:20:00 -0400

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <msp430.h> 
-
+#define GREEN_LED BIT0
+#define BUTTON1 BIT1
 
 /**
  * main.c
@@ -8,7 +9,7 @@ void main(void)
 {
 	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
 	
-	int a = 23;
+	/*int a = 23;
 	int b;
 	unsigned int c = 0xFFFF;
 	unsigned char d = 0x00;
@@ -20,5 +21,25 @@ void main(void)
 	a += 1;
 	b = 17/2;
 
-	while(1);
+	while(1);*/
+
+	P1OUT &= ~BIT0;
+	P1DIR |= BIT0;
+
+	P2DIR &= ~BIT3;
+	P2REN |= BIT3;
+	P2OUT |= BIT3;
+
+	PM5CTL0 &= ~LOCKLPM5;
+
+	int count = 0;
+
+	while(1){
+	    if((P4IN & BUTTON1) == 0x00){
+	        _delay_cycles(5000);
+	        if((P4IN & BUTTON1) == 0x00){
+	            P1OUT ^= GREEN_LED;
+	        } while ((P4IN & BUTTON1) == 0x00);
+	    }
+	}
 }
